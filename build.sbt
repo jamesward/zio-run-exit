4356334c932290273a29ccc93d2386e68dc440f8
James Ward <james@jamesward.com>
2023-05-15 14:14:11 -0600

    init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+scalaVersion := "3.2.2"
+
+libraryDependencies += "dev.zio" %% "zio" % "2.0.13"
+
+//run / fork := true
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+import zio.*
+
+object Main extends ZIOAppDefault:
+
+  override def run =
+    Console.printLine("hello, world")
